6c65f838df86948b6a985fd0a552d00ad3f438af
Myo Minn Oo <myominnoo@users.noreply.github.com>
2023-12-01 10:13:08 -0600

    test gha

---
+++ data_refresh.R
@@ -0,0 +1,11 @@
+
+
+library(tidyverse)
+library(RSocrata)
+
+df <- RSocrata::read.socrata("https://data.winnipeg.ca/resource/qd6b-q49i.json")
+print(df)
+
+str(df)
+
+readr::write_csv(x = df, file = "narcan.csv", append = FALSE)
---
+++ data_refresh.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo $1
+
+echo "Updating the total dataset"
+
+Rscript -e "source('data_refresh.R', echo = TRUE)"
+
+if [[ "$(git status --porcelain)" != "" ]]; then
+    git config --global user.name 'myominnoo'
+    git config --global user.email 'dr.myominnoo@gmail.com'
+    git add .
+    git commit -m "Update data"
+    git push
+fi
