81fc3d594457dd483e048d4f2f3e2ef5531c8ad7
Myo Minn Oo <myominnoo@users.noreply.github.com>
2023-12-01 11:34:16 -0600

    test save csv take5

--- data_refresh.R
+++ data_refresh.R
@@ -5,7 +5,6 @@ library(RSocrata)
 
 df <- RSocrata::read.socrata("https://data.winnipeg.ca/resource/qd6b-q49i.json")
 print(head(df))
-
 str(df)
 
 readr::write_csv(x = df, file = "data/narcan.csv", append = FALSE)
